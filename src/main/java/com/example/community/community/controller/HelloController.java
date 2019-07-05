785a44fb9889942ef44bedd6adbf89b6a68726a6
liuqi <1529599467@qq.com>
2019-07-05 11:46:12 +0800

    third

--- src/main/java/com/example/community/community/controller/HelloController.java
+++ src/main/java/com/example/community/community/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name")String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
 
     }
 }
